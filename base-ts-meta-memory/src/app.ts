5f8813afab082552189fec059786dd81f348e1dd
f0230 <f02301111@gmail.com>
2024-11-01 12:09:00 -0300

    first commit

---
+++ base-ts-meta-memory/src/app.ts
@@ -0,0 +1,22 @@
+import { createBot } from "@builderbot/bot";
+import { MemoryDB as DataBase } from "@builderbot/bot";
+import { provider } from "./provider";
+import { config } from "./config";
+import templates from "./templates";
+
+const PORT = config.PORT;
+
+const main = async () => { 
+    const { handleCtx, httpServer } = await createBot({
+        flow: templates,
+        provider: provider,
+        database: new DataBase(),
+    });
+
+
+
+    httpServer(+PORT);
+}
+
+main();
+
---
+++ base-ts-meta-memory/src/templates/index.ts
@@ -0,0 +1,8 @@
+import { createFlow } from "@builderbot/bot";
+import { mainFlow } from "./mainFlow";
+
+export default createFlow([
+    mainFlow
+]);
+
+
